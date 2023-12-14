e04d993b750d8ec59887b0e070b5d6e581c61e74
Miguel  de la Mata <sebidelamata@gmail.com>
2023-12-14 08:18:16 -0700

    fixed test

--- __tests__/Shop.test.jsx
+++ __tests__/Shop.test.jsx
@@ -2,13 +2,14 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from '@testing-library/react';
 import Shop from '../src/routes/Shop';
 import CustomRouter from "../src/routes/Router";
+import { BrowserRouter } from "react-router-dom";
 
 describe('Shop', () => {
     it('renders headline', () => {
         render(
-            <CustomRouter>
-                <Shop/>
-            </CustomRouter>
+           <BrowserRouter>
+            <Shop />
+           </BrowserRouter>
         ); 
 
         screen.debug();
@@ -16,11 +17,12 @@ describe('Shop', () => {
 
     it('renders correct heading', () => {
         render(
-            <CustomRouter>
+            <BrowserRouter>
                 <Shop/>
-            </CustomRouter>
+            </BrowserRouter>
         );
-        expect(screen.getByRole('heading').textContent).toMatch('Vite + React')
+
+        expect(screen.getByRole('heading').textContent).toMatch('Shop NFTs')
     })
     
 })
