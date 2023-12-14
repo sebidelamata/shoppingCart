0c65eb4147c14e02c2e9d465c2eeb6f7363e1267
Miguel  de la Mata <sebidelamata@gmail.com>
2023-12-13 23:11:11 -0700

    basic styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,29 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import PaperBoat from '../../public/PaperBoat.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <div>NavBar</div>
-        <Link to={'/'} className='navbar-link'>Home</Link>
-        <Link to={'/shop'} className='navbar-link'>Shop</Link>
-        <Link to={'/checkout'} className='navbar-link'>Checkout</Link>
+        <div className='navbar-name-and-logo'>
+            <div>
+                <img src={PaperBoat} className='icon-small' alt="GapingPond Logo Small" />
+            </div>
+            <div>
+                GapingPond
+            </div>
+        </div>
+        <ul className='navbar-list'>
+            <li className='navbar-list-item'>
+                <Link to={'/'} className='navbar-link'>Home</Link>
+            </li>
+            <li className='navbar-list-item'>
+                <Link to={'/shop'} className='navbar-link'>Shop</Link>
+            </li>
+            <li className='navbar-list-item'>
+                <Link to={'/checkout'} className='navbar-link'>Checkout</Link>
+            </li>
+        </ul>
     </div>
   )
 }
