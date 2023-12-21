d1fc2ad2ee543270b5a5f10f76d98ea0f5b4ad2f
Miguel  de la Mata <sebidelamata@gmail.com>
2023-12-20 22:29:12 -0700

    pagination for collections

--- src/components/AllCollections.jsx
+++ src/components/AllCollections.jsx
@@ -8,6 +8,7 @@ const AllCollections = () => {
         const [allCollectionsURL, setAllCollectionsURL] = useState(null)
         const [error, setError] = useState(null)
         const [loading, setLoading] = useState(true)
+        const [currentpage, setCurrentPage] = useState(0)
 
         const fetchAllCollections = async () => {
             const url = 'https://nft.llama.fi/collections';
@@ -36,16 +37,55 @@ const AllCollections = () => {
             fetchAllCollections()
         }, [])
     
-        return { allCollectionsURL, error, loading }
+        return { allCollectionsURL, error, loading, currentpage, setCurrentPage}
     }
 
-    const {allCollectionsURL, error, loading} = AllCollectionsURL()
+    const {allCollectionsURL, error, loading, currentpage, setCurrentPage} = AllCollectionsURL()
 
-
+    // wait for error/load screens
     if(error) return <p>A network error was encountered</p>
     if(loading) return <p>Loading...</p>
-    console.log(allCollectionsURL)
 
+    const numPages = Math.ceil(allCollectionsURL.length / 25)
+
+    const handlePaginationPageClick = (e) => {
+        let value = e.target.textContent - 1
+        setCurrentPage(value)
+        
+    }
+    const handlePaginationLeftArrowClick = () => {
+        let value = currentpage - 1
+        if(value < 0){
+            value = 0
+        }
+        setCurrentPage(value)
+    }
+    const handlePaginationRightArrowClick = () => {
+        let value = currentpage + 1
+        if(value > numPages - 1){
+            value = numPages -1
+        }
+        setCurrentPage(value)
+    }
+    const handlePaginationStartClick = () => {
+        setCurrentPage(0)
+    }
+    const handlePaginationEndClick = () => {
+        setCurrentPage(numPages - 1)
+    }
+
+    let paginationArray = Array.from({ length: 5}, (_, index) => {
+        let out = currentpage + index + 1
+        if(out > (numPages - 4)){
+            out = (numPages - 4) + index
+        }
+        return out
+    })
+    let startCollectionIndex = (25 * currentpage)
+    let endCollectionIndex = (25 * currentpage) + 24
+    if(endCollectionIndex > allCollectionsURL.length){
+        endCollectionIndex = allCollectionsURL.length
+    }
     return(
         <>
         <div className='shop-header-and-search'>
@@ -54,7 +94,7 @@ const AllCollections = () => {
         </div>
         <ul className='all-collections-list'>
             {
-                allCollectionsURL.slice(0,20).map((collection) => {
+                allCollectionsURL.slice(startCollectionIndex,endCollectionIndex).map((collection) => {
                         return (
                             <li key={collection.collectionId} className='all-collections-list-item'>
                                 <CollectionCard collection={collection}/>
@@ -63,6 +103,37 @@ const AllCollections = () => {
                 })
             }
         </ul>
+        <div className='pagination-row'>
+            <div className='pagination-row-start' onClick={() => handlePaginationStartClick()}>
+                Start
+            </div>
+            <div className='pagination-row-left-arrow' onClick={() => handlePaginationLeftArrowClick()}>
+                <strong>
+                    &#10594;
+                </strong>
+            </div>
+            <div className='pagination-div'>
+                <ul className='pagination-numbers-list'>
+                    {
+                        paginationArray.map((page) => {
+                            return (
+                                <li key={page} className='pagination-list-page' onClick={(e) => handlePaginationPageClick(e)}>
+                                    {page}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+            <div className='pagination-row-right-arrow' onClick={() => handlePaginationRightArrowClick()}>
+                <strong>
+                    &#10596;
+                </strong>
+            </div>
+            <div className='pagination-row-end' onClick={() => handlePaginationEndClick()}>
+                End
+            </div>
+        </div>
         </>
     )
 }
--- src/components/CollectionCard.jsx
+++ src/components/CollectionCard.jsx
@@ -1,7 +1,16 @@
 import { useState, useEffect } from 'react'
 import blank_nft from '../../public/blank_nft.svg'
+import eth_icon from '../../public/ethereum_icon.png'
 
 const CollectionCard = ({collection}) => {
+
+    const bullBearFontColor = (pctChange) => {
+        if(pctChange > 0){
+            return 'green'
+        } else if(pctChange < 0){
+            return 'red'
+        } else { return 'white'}
+    }
 
     return(
         <div className='collection-card'>
@@ -21,17 +30,99 @@ const CollectionCard = ({collection}) => {
 
                 )
             }
-            <div className='collection-card-name'><strong>{collection.name}</strong></div>
-            <div className='collection-card-address'>{collection.collectionId}</div>
-            <div className='collection-card-bottom-row'>
-                <div className='colllection-card-chain'></div>
+            <div className='collection-card-top-row'>
+                <div className='collection-card-name'><strong>{collection.name}</strong></div>
                 {
                     collection && (
                         <div className='collection-card-floor-price'>
-                            {`Floor Price: ${collection.floorPrice} ETH`}
+                            <div className='floor-price'>
+                                <div>
+                                    Floor Price:
+                                </div>
+                                <strong>
+                                    {` ${collection.floorPrice}`}
+                                </strong>
+                            </div>
+                            <div className='unit-of-payment'>
+                                <img className='unit-icon' src={eth_icon} alt="ETH" />
+                            </div>
                         </div>
                     )
                 }
+            </div>
+            <div className='collection-card-address'>{collection.collectionId}</div>
+            <div className='collection-card-bottom-row'>
+                <div className='colllection-card-chain'></div>
+            </div>
+            <div className='stats-rows'>
+                <div className='1-day-row'>
+                    {
+                        collection && (
+                            <div className='one-day-pct-change'>
+                                <div className='one-day-pct-change-label'>
+                                    1-Day Pct Change
+                                </div>
+                                <div className='one-day-pct-change-value-and-symbol'>
+                                    <div className='one-day-pct-change-value' style={{color: bullBearFontColor(collection.floorPricePctChange1Day)}}>
+                                        <strong>
+                                            {collection.floorPricePctChange1Day}
+                                        </strong>
+                                    </div>
+                                    <div className='one-day-pct-change-symbol' style={{ color: bullBearFontColor(collection.floorPricePctChange1Day)}}>
+                                        <strong>
+                                            %
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+                <div className='seven-day-row'>
+                    {
+                        collection && (
+                            <div className='seven-day-pct-change'>
+                                <div className='seven-day-pct-change-label'>
+                                    7-Day Pct Change
+                                </div>
+                                <div className='seven-day-pct-change-value-and-symbol'>
+                                    <div className='seven-day-pct-change-value' style={{color: bullBearFontColor(collection.floorPricePctChange7Day)}}>
+                                        <strong>
+                                            {collection.floorPricePctChange7Day}
+                                        </strong>
+                                    </div>
+                                    <div className='seven-day-pct-change-symbol' style={{ color: bullBearFontColor(collection.floorPricePctChange7Day)}}>
+                                        <strong>
+                                            %
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+                <div className='supply-and-for-sale-row'>
+                    <div className='supply'>
+                        <div className='supply-label'>
+                            Supply: 
+                        </div>
+                        <div>
+                            <strong>
+                                {collection.totalSupply}
+                            </strong>
+                        </div>
+                    </div>
+                    <div className='num-for-sale'>
+                        <div className='num-for-sale-label'>
+                            For Sale: 
+                        </div>
+                        <div>
+                            <strong>
+                                {collection.onSaleCount}
+                            </strong>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
