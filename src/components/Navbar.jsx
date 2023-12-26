5086834ca54417d47082c96e93ee49070dbd9e63
Miguel  de la Mata <sebidelamata@gmail.com>
2023-12-26 15:33:17 -0700

    wallet css stuff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,30 +6,32 @@ import WalletConnectButton from './WalletConnectButton'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <Link to={'/'} className='navbar-name-and-logo'>
-            <div>
-                <img src={PaperBoat} className='icon-small' alt="GapingPond Logo Small" />
-            </div>
-            <div>
-                GapingPond
-            </div>
-        </Link>
-        <ul className='navbar-list'>
-        <li className='navbar-list-item'>
-                <w3m-button/>
-            </li>
-            <li className='navbar-list-item'>
-                <Link to={'/shop'} className='navbar-link'>Shop</Link>
-            </li>
-            <li className='navbar-list-item'>
-                <Link to={'/checkout'} className='navbar-link'>
-                    <i className="fas fa-shopping-cart"></i>
-                </Link>
-            </li>
-            <li className='navbar-list-item'>
-                <Link to={'/docs'} className='navbar-link'>Docs</Link>
-            </li>
-        </ul>
+        <div className='navbar-row-1'>
+            <Link to={'/'} className='navbar-name-and-logo'>
+                <div>
+                    <img src={PaperBoat} className='icon-small' alt="GapingPond Logo Small" />
+                </div>
+                <div>
+                    GapingPond
+                </div>
+            </Link>
+            <ul className='navbar-list'>
+                <li className='navbar-list-item'>
+                    <Link to={'/shop'} className='navbar-link'>Shop</Link>
+                </li>
+                <li className='navbar-list-item'>
+                    <Link to={'/checkout'} className='navbar-link'>
+                        <i className="fas fa-shopping-cart"></i>
+                    </Link>
+                </li>
+                <li className='navbar-list-item'>
+                    <Link to={'/docs'} className='navbar-link'>Docs</Link>
+                </li>
+            </ul>
+        </div>
+        <div className='navbar-row-2'>
+            <w3m-button/>
+        </div>
     </div>
   )
 }
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -25,18 +25,18 @@ function App() {
 
   return (
     <WagmiConfig config={wagmiConfig}>
-      <div>
+      <div className='home-page'>
         <Navbar />
         <div className='body'>
-          <div>
+          <div className='logo-large-container'>
             <img className='icon-large' src={Paperboat} alt="GapingPond Logo" />
           </div>
+          <h1>GapingPond</h1>
+          <div className="card">
+            <div>A Custom Whitelabel NFT Marketplace</div>
+            <div>bc sometimes its better to be a big fish in a small pond</div>
+          </div>
         </div>
-      </div>
-      <h1>GapingPond</h1>
-      <div className="card">
-        <div>A Custom Whitelabel NFT Marketplace</div>
-        <div>bc sometimes its better to be a big fish in a small pond</div>
       </div>
     </WagmiConfig>
   )
