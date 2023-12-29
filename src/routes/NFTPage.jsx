f706a3f598eb1de7107b71aaefdb856f05f4ff22
Miguel  de la Mata <sebidelamata@gmail.com>
2023-12-29 09:09:06 -0700

    traits card

--- src/routes/NFTPage.jsx
+++ src/routes/NFTPage.jsx
@@ -8,6 +8,11 @@ const NFTPage = () => {
 
     const location = useLocation()
     const [openSeaSingleNFTData, collection] = location.state || {}
+    const [showTraits, setShowTraits] = useState(false)
+
+    const handleTraitsClick = () => {
+        setShowTraits(!showTraits)
+    }
 
     console.log(openSeaSingleNFTData)
 
@@ -31,8 +36,50 @@ const NFTPage = () => {
                             <strong>{openSeaSingleNFTData.nft.name || `${collection.collection.name} #${openSeaSingleNFTData.nft.identifier}`}</strong>
                         </div>
                         <img className="nft-page-nft-image" src={openSeaSingleNFTData.nft.image_url} alt="NFT Image" />
+                        {
+                            openSeaSingleNFTData &&
+                            openSeaSingleNFTData.nft.owners &&
+                            (
+                                <a className="owner" href={`https://etherscan.io/address/${openSeaSingleNFTData.nft.owners[0].address}`} target="_blank" rel="noreferrer">
+                                    <button><strong>Owner</strong></button>
+                                </a>
+                            )
+                        }
                     </div>
                 }
+                <div className="traits-container" onClick={() => handleTraitsClick()}>
+                    <div className="traits-title">
+                        <strong>Traits</strong>
+                    </div>
+                    {   
+                        showTraits === true &&
+                        <ul className="traits-list">
+                            {
+                                openSeaSingleNFTData.nft.traits.map((trait) => {
+                                    return(
+                                        <li key={trait.trait_type} className="trait-card">
+                                            <div className="trait-title">
+                                                <strong>{trait.trait_type}</strong>
+                                            </div>
+                                            {
+                                                trait.value &&
+                                                <div className="trait-value">
+                                                    {trait.value}
+                                                </div>
+                                            }
+                                            {
+                                                trait.trait_count &&
+                                                <div className="trait-count">
+                                                    {((trait.trait_count / collection.collection.totalSupply) * 100).toFixed(2)}%
+                                                </div>
+                                            }
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    }
+                </div>
             </div>
             <NFTPageListings openSeaSingleNFTData={openSeaSingleNFTData}/>
             <NFTPageOffers openSeaSingleNFTData={openSeaSingleNFTData}/>
