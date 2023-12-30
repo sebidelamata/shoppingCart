5bcab8be3df6c77854ca6fcc0b3fb79fdbc96b5a
Miguel  de la Mata <sebidelamata@gmail.com>
2023-12-29 19:36:34 -0700

    counter 1/100s of second

--- src/components/ListingStopwatch.jsx
+++ src/components/ListingStopwatch.jsx
@@ -1,23 +1,29 @@
 import { useState, useEffect } from "react";
 
 const ListingStopwatch = ({listingInfo}) => {
-    
-    const timeRemaining = listingInfo.slice().reverse()[0].expiration_time - (Date.now() / 1000).toFixed(0)
 
-    const [timeLeft, setTimeLeft] = useState(timeRemaining)
+    const loadPage = () => {
+        const timeRemaining = ((listingInfo.slice().reverse()[0].expiration_time * 100) - Math.floor(Date.now() / 10)) / 100
+        console.log(timeRemaining)
+        const [timeLeft, setTimeLeft] = useState(timeRemaining)
 
-    useEffect(() => {
-        const key = setInterval(() => {
-            setTimeLeft(count => count - 1)
-          }, 1000);
-          return () => {
+        useEffect(() => {
+            const key = setInterval(() => {
+                setTimeLeft(count => count - 0.01)
+              }, 10);
+            return () => {
             clearInterval(key);
-          };
-    },[])
+            };
+        },[])
+
+        return{timeLeft}
+    }
+
+    const{timeLeft} = loadPage()
 
     return(
         <div>
-            {timeLeft}
+            {timeLeft.toFixed(2)}
         </div>
     )
 
